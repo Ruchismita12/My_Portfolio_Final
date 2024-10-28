ee58e0cfc7574ed3233ac3a8e2f52fac9973498e
Ruchismita12 <138961626+Ruchismita12@users.noreply.github.com>
2024-10-28 19:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Software Developer Intern",
+    "C++ Developer",
+    "Full-stack Web Developer",
+  ],
+  typeSpeed: 10,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
